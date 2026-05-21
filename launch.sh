ae2f291868b44f8cd8ebdb37f7f2f367d9712792
danguetta <daniel@guetta.com>
2026-05-21 19:44:33 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,10 @@
+#    IMPORTANT - GO TO THE VERY BOTTOM TO ADD EXTRA ADMIN USERS
+ADMIN_USER='x05_07_w331081xxdanielxguetta'
+
+# Set up the links
+URL_TLJF_INSTALLER='https://tljh.jupyter.org/bootstrap.py'
+URL_REQUIREMENTS='https://www.dropbox.com/scl/fi/khb85vvdtmn3cwzomxy7c/requirements.txt?rlkey=9uy9rjangw63f7nqywovo7n88&st=okgq5e53&dl=1'
+
 
 # Install jupyterlab (the URL points to a requirements.txt file)
 curl -L "$URL_TLJF_INSTALLER" | sudo python3 - --admin "$ADMIN_USER" --show-progress-page --user-requirements-txt-url "$URL_REQUIREMENTS"
